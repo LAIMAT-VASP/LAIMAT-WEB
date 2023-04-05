b7f2167495de515787fb398ee394d7d21f0cb911
adrien <adrien.bony38@gmail.com>
2023-04-05 16:06:39 +0200

    FEATURE: appels api

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,34 @@
+const apiUrl = "http://localhost:5000/"; // URL de l'API
+
 function calculerHypotenuse() {
   // Récupérer les valeurs des côtés
-  var cote1 = parseInt(document.getElementById("cote1").value);
-  var cote2 = parseInt(document.getElementById("cote2").value);
-  var hypotenuse = Math.sqrt(cote1 * cote1 + cote2 * cote2);
+  let cote1 = parseInt(document.getElementById("cote1").value);
+  let cote2 = parseInt(document.getElementById("cote2").value);
 
-  // Arrondir le résultat à 2 décimales
-  hypotenuse = Math.round(hypotenuse * 100) / 100;
+  axios
+    .get(apiUrl + "hypothenuse?isJson=true&a=" + cote1 + "&b=" + cote2)
+    .then((response) => {
+      let hypotenuse = response.data.hypothenuse;
 
-  // Afficher le résultat dans la page
-  document.getElementById("hypotenuse").value = hypotenuse;
-  document.getElementById("cote-triangle1").innerHTML = cote1;
-  document.getElementById("cote-triangle2").innerHTML = cote2;
-  document.getElementById("cote-triangle3").innerHTML = hypotenuse;
+      // Arrondir le résultat à 2 décimales
+      hypotenuse = Math.round(hypotenuse * 100) / 100;
+
+      // Afficher le résultat dans la page
+      document.getElementById("hypotenuse").value = hypotenuse;
+      document.getElementById("cote-triangle1").innerHTML = cote1;
+      document.getElementById("cote-triangle2").innerHTML = cote2;
+      document.getElementById("cote-triangle3").innerHTML = hypotenuse;
+    });
 }
 
-var canvas = document.getElementById("canvas");
+let canvas = document.getElementById("canvas");
 canvas.width = 400;
 canvas.height = 300;
 
-var object = new Image();
+let object = new Image();
 object.src = "images/ok.png";
 
-var position = {
+let position = {
   x: canvas.width / 2 - object.width / 2,
   y: -object.height / 2,
 };
@@ -46,7 +53,7 @@ function animate() {
       vitesseAnimation = 0;
     }
   }
-  var ctx = canvas.getContext("2d");
+  let ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(object, position.x - 100, position.y);
 }
@@ -69,14 +76,17 @@ function calculVitesseDepuishauteur() {
     return;
   }
 
-  var hauteur = parseInt(document.getElementById("hauteur").value);
-  var vitesse = Math.sqrt(2 * hauteur * 9.81);
-  let vitesseKmh = vitesse * 3.6;
-  vitesseKmh = Math.round(vitesseKmh * 100) / 100;
-  document.getElementById("vitesse").value = vitesseKmh + " km/h";
-  document.getElementById("hauteurDepart").innerHTML = hauteur;
-  document.getElementById("hauteurDepartMoitie").innerHTML = hauteur / 2;
+  let hauteur = parseInt(document.getElementById("hauteur").value);
 
-  stopAnimation();
-  animate();
+  axios.get(apiUrl + "speed?isJson=true&height=" + hauteur).then((response) => {
+    let vitesse = response.data.speedInKilometers;
+    let vitesseKmh = Math.round(vitesse * 100) / 100;
+
+    document.getElementById("vitesse").value = vitesseKmh + " km/h";
+    document.getElementById("hauteurDepart").innerHTML = hauteur;
+    document.getElementById("hauteurDepartMoitie").innerHTML = hauteur / 2;
+
+    stopAnimation();
+    animate();
+  });
 }
